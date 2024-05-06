4d3a6c3eec78588d079f1367b58fedc343c4ea59
thzgyesmasther <3064531661@qq.com>
2024-05-06 17:23:26 +0800

    deleteActivityRemark and updateActivityRemark

--- crm/src/main/java/com/bjpowernode/crm/workbench/web/controller/ActivityRemarkController.java
+++ crm/src/main/java/com/bjpowernode/crm/workbench/web/controller/ActivityRemarkController.java
@@ -51,5 +51,52 @@ public class ActivityRemarkController {
         return returnObject;
     }
 
+    @RequestMapping("/workbench/activity/deleteActivityRemarkById.do")
+    @ResponseBody
+    public Object deleteActivityRemarkById(String id){
+        ReturnObject returnObject = new ReturnObject();
 
+        try {
+            int ret = activityRemarkService.deleteActivityRemarkById(id);
+            if(ret > 0){
+                returnObject.setCode(Contants.RETURN_OBJECT_CODE_SUCCESS);
+            }else{
+                returnObject.setCode(Contants.RETURN_OBJECT_CODE_FAIL);
+                returnObject.setMessage("系统繁忙...");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            returnObject.setCode(Contants.RETURN_OBJECT_CODE_FAIL);
+            returnObject.setMessage("系统繁忙...");
+        }
+
+        return returnObject;
+    }
+
+    @RequestMapping("/workbench/activity/saveEditActivityRemark.do")
+    @ResponseBody
+    public Object saveEditActivityRemark(ActivityRemark remark , HttpSession session){
+        remark.setEditTime(DateUtils.formateDateTime(new Date()));
+        User user = (User) session.getAttribute(Contants.SESSION_USER);
+        remark.setEditBy(user.getName());
+        remark.setEditFlag(Contants.REMARK_EDIT_FLAG_YES_EDITED);
+
+        ReturnObject returnObject = new ReturnObject();
+        try {
+            int ret = activityRemarkService.saveEditActivityRemark(remark);
+            if(ret > 0){
+                returnObject.setCode(Contants.RETURN_OBJECT_CODE_SUCCESS);
+                returnObject.setRetData(remark);
+            }else{
+                returnObject.setCode(Contants.RETURN_OBJECT_CODE_FAIL);
+                returnObject.setMessage("系统繁忙...");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            returnObject.setCode(Contants.RETURN_OBJECT_CODE_FAIL);
+            returnObject.setMessage("系统繁忙...");
+        }
+
+        return returnObject;
+    }
 }
